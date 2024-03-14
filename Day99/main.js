c41c27b40857188c7565de92c3298580af080056
Marderon123 <alexmonu2000@gmail.com>
2024-03-14 16:14:57 +0100

    Added stripes

---
+++ Day99/main.js
@@ -0,0 +1,18 @@
+const stripes = document.querySelector('.stripes')
+
+for (let i = 1; i <= 18; i++) {
+    stripes.innerHTML += `<div class="stripe stripe-${i}"></div>`
+}
+
+
+for (let i = 1; i <= 18; i++) {
+    const stripe = document.querySelector(`.stripe-${i}`)
+
+    stripe.style.transform = `rotate(${i * 20}deg) translateY(-120px)`
+}
+
+
+const label = document.querySelector('.label')
+label.addEventListener('click', () => {
+    document.querySelector('.frame').classList.toggle('checked')
+})
