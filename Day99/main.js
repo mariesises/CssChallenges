879d5f4a6eaf7219e882ccd6cee0985b224feaa4
Marderon123 <alexmonu2000@gmail.com>
2024-03-15 04:05:20 +0100

    Circle-Square - Day 99

--- Day99/main.js
+++ Day99/main.js
@@ -1,13 +1,12 @@
 const stripes = document.querySelector('.stripes')
 
-for (let i = 1; i <= 18; i++) {
+for (let i = 1; i <= 20; i++) {
     stripes.innerHTML += `<div class="stripe stripe-${i}"></div>`
 }
 
 
-for (let i = 1; i <= 18; i++) {
+for (let i = 1; i <= 20; i++) {
     const stripe = document.querySelector(`.stripe-${i}`)
-
     stripe.style.transform = `rotate(${i * 20}deg) translateY(-120px)`
 }
 
